4052dbc8e3306967c0daf0bd7f44f98b25cc9c18
guiiihhmatos <gui.santana.matos@gmail.com>
2023-06-02 16:48:27 -0300

    IMPROVEMENT - tabela resolvida

--- includes/components/header.php
+++ includes/components/header.php
@@ -5,10 +5,13 @@
   if (isset($_POST['logout'])) {
     logout();
   }
+
+  $username = $_SESSION['username'];
 ?>
 
 <head>
   <link rel="stylesheet" href="../public/assets/css/header.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <div class="header__list">
@@ -18,7 +21,11 @@
       <li style="margin-left: 20px;"><a href="./Tarefas_log.php">Minhas Tarefas</a></li>
     </ul>
 
-    <form method="POST">
+    <form method="POST" class="usuario__Pai">
+      <div class="usuario">
+        <i class="bi bi-person-fill" style="color: white;"></i>
+        <p style="color: white; margin: 0 10px; font-size: 18px;"><?= $username ?></p>
+      </div>
       <button class="botao-logout" type="submit" name="logout">Logout</button>
     </form>
 </div>
